e9096cfdab595e86feb2a12e2c2b95c78949f9e1
Cookies745 <joshualimyonjian@gmail.com>
2026-03-10 11:47:02 +0000

    Updated main.py to host at 0.0.0.0 and updated Port.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi_mcp import FastApiMCP
 from router import userCRUD, bookCRUD, extraFeatures
+import os
 
 app = FastAPI(title="Book Engine API")
 
@@ -25,7 +26,8 @@ mcp = FastApiMCP(
 )
 
 mcp.mount_http()
+PORT = os.environ.get("PORT", 8000)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
